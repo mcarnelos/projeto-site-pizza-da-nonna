cad500b22bdb8be91837dd0e34355d64f731f605
Murilo Carnelos <muriloc.c@hotmail.com>
2022-01-27 19:59:23 -0300

    Inclusao do resumo do pedido

---
+++ css/script.js
@@ -0,0 +1,13 @@
+let inputNome = document.getElementById("nome");
+let inputTelefone = document.getElementById("telefone");
+let inputEndereco = document.getElementById("endereco");
+
+let resumoPedido = document.getElementById("resumo-do-pedido");
+
+function fazerPedido(){
+    console.log("Fazendo pedido...");
+
+    let nome = inputNome.value;
+    let telefone = inputTelefone.value;
+    let endereco = inputEndereco.value;
+}
